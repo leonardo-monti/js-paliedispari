fe3d59401a18f64a7bf1ff840741e7ea47d01ddc
leonardo-monti <monti.leonardo22@gmail.com>
2026-01-08 18:25:38 +0100

    add funcioton

--- pariedispari.js
+++ pariedispari.js
@@ -5,4 +5,17 @@ const numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
 function randomNumber() {
     return Math.floor(Math.random() * 5) + 1;
 }
+const numeroCpu = randomNumber();
 
+const somma = numeroUtente + numeroCpu
+
+function isPari(numero) {
+    if (numero % 2 === 0) {
+        return "pari";
+    }
+    else {
+        return "dispari";
+    }
+}
+
+const risultato = isPari(somma);
