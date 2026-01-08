eda150c6b5033d6d9ca49e023cea56e80ebf76f7
leonardo-monti <monti.leonardo22@gmail.com>
2026-01-08 16:53:52 +0100

    end

--- pali.js
+++ pali.js
@@ -1,18 +1,23 @@
 const parolaUtente = prompt("Inserisci una parola");
 
-function isPalindroma(parola) {
-    const parolaInversa = parola.split("").reverse().join("");
-    if (parola === parolaInversa) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-const inserimento = isPalindroma(parolaUtente);
-
-if (inserimento) {
-    alert("La parola è palindroma");
+if (!isNaN(parolaUtente)) {
+    alert("I numeri NON sono permessi");
 } else {
-    alert("La parola NON è palindroma");
+
+    function isPalindroma(parola) {
+        const parolaInversa = parola.split("").reverse().join("");
+        if (parola === parolaInversa) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    const inserimento = isPalindroma(parolaUtente);
+
+    if (inserimento) {
+        alert("La parola è palindroma");
+    } else {
+        alert("La parola NON è palindroma");
+    }
 }
