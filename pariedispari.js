1cdc229f652f98d44fbb3a33bdbf167b71039202
leonardo-monti <monti.leonardo22@gmail.com>
2026-01-08 17:57:29 +0100

    start pariedispari

---
+++ pariedispari.js
@@ -0,0 +1,8 @@
+const sceltaUtente = prompt("Pari o dispari?");
+
+const numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
+
+function randomNumber() {
+    return Math.floor(Math.random() * 5) + 1;
+}
+
